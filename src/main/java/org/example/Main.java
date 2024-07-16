30458ab75b9505826a0f1860780cc57e2490927a
id <1773013849@qq.com>
2024-07-16 22:19:19 +0800

    项目创建

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.sun.xml.internal.ws.api.server.SDDocumentFilter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
